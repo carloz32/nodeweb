21252735da6fce6228cb9ae8e63f3b6969b27a8d
carloz32 <carlosgr822@gmail.com>
2019-03-21 13:59:09 -0600

    implementación de datos por formulario

--- src/app.js
+++ src/app.js
@@ -2,12 +2,14 @@ var express = require('express')
 var app = express()
 const path = require('path');
 const hbs = require('hbs');
+const bodyParser = require('body-parser')
 require ('./helpers');
 
 const directoriopublico = path.join(__dirname, '../public');
 const directoriopartial = path.join(__dirname, '../partials');
 app.use(express.static(directoriopublico));
 hbs.registerPartials(directoriopartial);
+app.use(bodyParser.urlencoded({extended: false}))
 app.set('view engine', 'hbs');
 app.get('/', (req, res) => {
     res.render('index', {
@@ -15,15 +17,21 @@ app.get('/', (req, res) => {
     });
 });
 
-app.get('/calculos', (req, res) => {
-    console.log(req.query);
+app.post('/calculos', (req, res) => {
+
     res.render('calculos', {
-        estudiante: req.query.nombre,
-        nota1: parseInt(req.query.nota1),
-        nota2: parseInt(req.query.nota2),
-        nota3: parseInt(req.query.nota3)
+        estudiante: req.body.nombre,
+        nota1: parseInt(req.body.nota1),
+        nota2: parseInt(req.body.nota2),
+        nota3: parseInt(req.body.nota3)
     });
 });
+
+app.get('*',(req,res)=>{
+    res.render('Error'),{
+        estudiante: 'error'
+    }
+})
 
 app.listen(3000, () => {
     console.log('Escuchando en el puerto 3000');
